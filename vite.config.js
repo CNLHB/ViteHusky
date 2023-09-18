3be34d621f8b24dd74ce7feb2c871b0ed6bc9d1f
爱华 <1348844909@qq.com>
2023-09-18 19:15:34 +0800

    💄style(styles): 格式化代码

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    eslintPlugin({
+      failOnError: false, //报错不影响编译
+      failOnWarning: false, // 警告不影响编译
+      include: ['src/**/*.js', 'src/**/*.vue']
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  lintOnSave: true
+});
